5cda1c199bedfa39e7f637986a6bc0ccd24eb47b
Sandeep Kaushik <56245081+Shyamsandeep28@users.noreply.github.com>
2026-06-14 18:15:43 +0530

    Update LoginServlet.java

--- src/main/java/webapp/LoginServlet.java
+++ src/main/java/webapp/LoginServlet.java
@@ -42,6 +42,7 @@ public class LoginServlet extends HttpServlet {
 	out.println("</head>");
 		out.println("<body>");
 		out.println("<h1> welcome to Devops Training</h1>");
+		out.println("<h1> This is my updated app </h1>");
 		
 
 				out.println("</body>");
